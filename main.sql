9b910bf1adb3cfb94951b3a5af835e6c4160a92a
Ian Skelskey <ianskelskey@gmail.com>
2020-03-11 03:09:01 +0000

    Added States and Heights

--- main.sql
+++ main.sql
@@ -11,12 +11,12 @@ CREATE TABLE mountainsDB (
 INSERT INTO mountainsDB VALUES
   ('Bear Mountain', 'CT', 2323, '1953-05-29'),
   ('Mount Rogers', 'VA', 5729, '1889-10-06'),
-  ('Mount Greylock', 3489, '1913-06-07'),
-  ('Mount Marcy', 5343, '1880-01-04'),
-  ('Mount Mansfield', 8611, '1954-07-31'),
-  ('Mount Washington', 3900, '1835-08-12');
+  ('Mount Greylock', 'MA', 3489, '1913-06-07'),
+  ('Mount Marcy', 'NY', 5343, '1880-01-04'),
+  ('Mount Mansfield', 'VT', 4395, '1954-07-31'),
+  ('Mount Washington', 'NH', 6288, '1835-08-12');
 
-.print 'average mountain height'
+.print 'Average Mountain Height'
 SELECT avg(height_meters) AS avg_height
 FROM mountainsDB;
 
